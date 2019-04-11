4b6229f0803de7f099586d68eb1b8e4973857c84
Gabriele <gabrielecorso95@gmail.com>
2019-04-11 18:23:10 +0200

    feat: added agid template

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'fare.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'agid_template',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
