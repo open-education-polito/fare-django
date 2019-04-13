ab80e0e8e3b40012b940293e664b922a6df5c096
libremente <surf@libremente.eu>
2019-04-13 19:28:00 +0200

    add: flake8 to cci

--- fare/taskapp/celery.py
+++ fare/taskapp/celery.py
@@ -26,8 +26,6 @@ class CeleryAppConfig(AppConfig):
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
 
-        
-
 
 @app.task(bind=True)
 def debug_task(self):
