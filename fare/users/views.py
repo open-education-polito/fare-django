a6b2f0eed4d65eb9a86f4ad9c91dff87d6d47e0f
Gabriele <gabrielecorso95@gmail.com>
2019-04-13 12:23:19 +0200

    feat: added the documentation

--- fare/users/views.py
+++ fare/users/views.py
@@ -8,6 +8,23 @@ User = get_user_model()
 
 
 class ChangeStaffPermissionView(LoginRequiredMixin, UpdateView):
+    """
+    This class allows to change the staff_member permission of a user
+
+    :attr http_methods_names: allowed http methods for this class
+    :type http_methods_names: list
+    :attr model: reference model used by this class
+    :type model: User
+    :attr slug_field: store and generate a valid url form the username of the requested user
+    :type slug_field: string
+    :attr slug_url_kwarg: tell the view to get the url kwarg named as specified in slug_field
+    :type slug_url_kwarg: dictionary
+    :attr fields: fields that can be modified
+    :type fields: set
+    :attr template_name_suffix: suffix that indicates the right template
+    :type template_name_suffix: string
+
+    """
     http_method_names = ['post', 'get']
     model = User
     slug_field = "username"
@@ -16,6 +33,10 @@ class ChangeStaffPermissionView(LoginRequiredMixin, UpdateView):
     template_name_suffix = '_change_permission'
 
     def get(self, request, *args, **kwargs):
+        """
+        Manage the GET request, if the user has the permission and the requested user is not an admin,
+        display the page where the he can modify the requested user's permission 
+        """
         # verify if the user is a staff member and the requested user is not an admin
         if request.user.staff_member and not User.objects.get(username=kwargs['username']).is_superuser:
             return super().get(request, *args, **kwargs)
@@ -23,6 +44,10 @@ class ChangeStaffPermissionView(LoginRequiredMixin, UpdateView):
             return HttpResponseRedirect(reverse("home"))
 
     def post(self, request, *args, **kwargs):
+        """
+        Manage the POST request, check the permission and if the user has it, 
+        change the field of the requested user unless he is an admin
+        """
         # verify if the user is a staff member and the requested user is not an admin
         if request.user.staff_member and not User.objects.get(username=kwargs['username']).is_superuser:
             return super().post(request, *args, **kwargs)
@@ -30,6 +55,9 @@ class ChangeStaffPermissionView(LoginRequiredMixin, UpdateView):
             return HttpResponseRedirect(reverse("home"))
 
     def get_success_url(self):
+        """
+        url where the user is redirected after a successful operation
+        """
         return reverse("users:list")
 
 
