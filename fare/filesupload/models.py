ce0071b0662795df812f1c23794a8b0dd4fab233
Gabriele <gabrielecorso95@gmail.com>
2019-05-14 19:29:10 +0200

    feat: created the document model

---
+++ fare/filesupload/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    school_level = models.CharField(max_length=255)
+    school_subject = models.CharField(max_length=255)
