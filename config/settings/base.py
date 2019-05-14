2088d4f90b4e5613ff8d3664d5018ee701567edd
Gabriele <gabrielecorso95@gmail.com>
2019-05-14 19:19:04 +0200

    feat: installed filesupload app

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,6 +70,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'fare.users.apps.UsersAppConfig',
+    'fare.filesupload.apps.FilesuploadConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
