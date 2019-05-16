44d15839b1414317efa41652590135ed0c9ea26f
Eugenio <eugeniocorso95@gmail.com>
2019-05-16 18:24:17 +0200

    feat: added view for uploading a file in view.py

--- fare/filesupload/views.py
+++ fare/filesupload/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import DocumentForm
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+class FileFieldView(FormView):
+    form_class = DocumentForm
+    template_name = 'filesupload.html'
+    success_url = 'home'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
